50a0988ac2b27333a9e0298696613ca11b69f279
pavanibandaru-1228 <pavanibandaru032@gmail.com>
2025-03-13 15:10:57 +0530

    Create tribute.js

---
+++ tribute.js
@@ -0,0 +1,5 @@
+document.getElementById("showMessage").addEventListener("click", 
+function() { 
+document.getElementById("tributeMessage").innerText = "Saluting the bravery 
+of Captain Vikram Batra! Your sacrifice will never be forgotten. Jai Hind!"; 
+}); 
